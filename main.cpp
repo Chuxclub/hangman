933c1a4c81c81f5f16a8ba783b7ca9ea5460aaea
Florian <florian-1992@hotmail.fr>
2018-08-09 15:06:00 +0200

    Word picker function done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "std_lib_facilities.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> dictionary;
+    fstream file("/home/florian/CLionProjects/hangman/words_alpha.txt");
+    std::string words;
+
+    while(file >> words)
+    {
+        dictionary.push_back(words);
+    }
+
+    srand((unsigned int) time(nullptr));
+    int i = rand()%370099;
+
+    std::cout << dictionary[i] << "\n";
+
     return 0;
 }

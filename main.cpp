2d29ae8057947b7b725e52604281674e7a0bbfac
Florian <florian-1992@hotmail.fr>
2018-08-10 16:13:00 +0200

    Core game functionnal, need clean-up

--- main.cpp
+++ main.cpp
@@ -2,22 +2,65 @@
 #include "std_lib_facilities.h"
 
 string word_picker();
-vector <size_t> occurrence_finder(string picked, char guess);
+//Return random picked word
+
+string word_concealer(string picked);
+//Conceals random picked word
+
+vector <size_t> occurrence_finder(string picked, string guess);
+//Compares user char with chars in picked word
+//Returns a vector with the positions of chars in string
+
+/*string word_revealer(vector <size_t> a, string concealed, string guess);*/
+
+
+/* ---------------------------------------------- HANGMAN GAME SOLO ---------------------------------------------- */
 
 int main()
 {
+    bool won = false;
+
     string picked_word = word_picker();
 
+    string concealed_word = word_concealer(picked_word);
+    cout << concealed_word << "\n\n";
+
     std::cout << "Pick a letter: ";
+    string user_letter_guess;
 
-    char user_letter_guess = 'o';
-    std::cin >> user_letter_guess;
+    while(!won)
+    {
+        std::cin >> user_letter_guess;
+        std::cout << "\n";
+
+        vector <size_t> occurrence_pos = occurrence_finder(picked_word, user_letter_guess);
+
+        for(size_t x : occurrence_pos)
+            concealed_word.replace(x, 1, user_letter_guess);
+
+        cout << concealed_word << "\n\n";
+
+        for (char x : concealed_word)
+        {
+            if(x == '*')
+            {
+                won = false;
+                std::cout << "Pick a letter: ";
+                break;
+            }
+
+            won = true;
+        }
+    }
+
+    std::cout << "You won!";
     std::cout << "\n";
-
-    occurrence_finder(picked_word, user_letter_guess);
 
     return 0;
 }
+
+/* ---------------------------------------------------------------------------------------------------------------- */
+
 
 string word_picker()
 {
@@ -35,30 +78,33 @@ string word_picker()
     int x = rand()%TOTAL_OF_WORDS;
     std::string picked_word = dictionary[x];
 
-    for(int i = 0; i < picked_word.length(); i++)
-        std::cout << "_ ";
-
-    std::cout << "\n\n";
-
     return picked_word;
 }
 
-vector <size_t> occurrence_finder(string picked, char guess)
+
+string word_concealer(string picked)
+{
+    std::string concealed_word;
+
+    for(int i = 0; i < picked.length(); i++)
+        concealed_word.push_back('*'); //Temporary solution to not bother with blank spaces
+
+    return concealed_word;
+}
+
+vector <size_t> occurrence_finder(string picked, string guess)
 {
     vector <size_t> founds_vec;
     size_t found = picked.find(guess);
 
     if (found == std::string::npos)
-        std::cout << "This letter isn't in my word!" << "\n";
+        std::cout << "Try again!" << "\n\n";
 
     else
     {
-        while(found != std::string::npos) //Doesn't work at all! Don't run!
+        while(found != std::string::npos)
         {
             founds_vec.push_back(found);
-
-            std::cout << picked.at(found);
-
             found = picked.find(guess, found + 1);
         }
     }
@@ -66,5 +112,17 @@ vector <size_t> occurrence_finder(string picked, char guess)
     return founds_vec;
 }
 
+/*string word_revealer(vector <size_t> a, string concealed, string guess)
+{
+    for(size_t x : a)
+        concealed.replace(x, 1, guess);
+
+    return concealed;
+}*/
+
 
 //std::cout << picked_word << "\n\n";
+//concealed.replace(x, 1, guess);
+//concealed.erase(x, 1);
+//concealed.insert(x, guess);
+//string revealing_word;
